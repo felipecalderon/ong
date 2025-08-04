e210ec4840bc55320355fef76f5722fe8399184c
Felipe <felipe.calderon321@gmail.com>
2025-08-04 16:50:31 -0400

    fix postlist

--- src/components/posts/PostList.tsx
+++ src/components/posts/PostList.tsx
@@ -1,24 +1,22 @@
-
 import { getPosts } from '@/actions/post.action'
-import { Post } from '@/interfaces/post.interface';
 
 interface PostListProps {
-    postType: 'noticia' | 'denuncia' | 'recomendacion';
-    userId?: string;
+    postType: 'noticia' | 'denuncia' | 'recomendacion'
+    userId?: string
 }
 
 export default async function PostList({ postType, userId }: PostListProps) {
-    const posts: Post[] = await getPosts({ postType, userId });
+    const { data } = await getPosts({ postType, userId })
 
-    if (!posts.length) {
-        return <div>No se han encontrado publicaciones de tipo {postType}</div>;
+    if (!data) {
+        return <div>No se han encontrado publicaciones de tipo {postType}</div>
     }
 
     return (
         <div>
-            {posts.map((post) => (
-                <div key={post._id} className="ProseMirror" dangerouslySetInnerHTML={{ __html: post.content }} />
+            {data.map((post) => (
+                <div key={post._id} className='ProseMirror' dangerouslySetInnerHTML={{ __html: post.content }} />
             ))}
         </div>
-    );
+    )
 }
